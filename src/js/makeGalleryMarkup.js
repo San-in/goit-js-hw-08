c72873879b97d8537cbbcdc0a94d631db9ff32a7
Sanin Oleksandr <saninalexsanin@gmail.com>
2023-01-30 16:23:09 +0200

    fixed bugs

--- src/js/makeGalleryMarkup.js
+++ src/js/makeGalleryMarkup.js
@@ -1,1 +1,4 @@
-export default makeGalleryMarkup = array => array.map(({original,preview,description}) => `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>`).join('');
+export default function makeGalleryMarkup(array) {
+    return array.map(({original,preview,description}) => `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}"/></a>`).join('');
+}
